8fe33492b14443cb1bb638b5da12a6d628f44ca0
krsdl <35543755+Kresdl@users.noreply.github.com>
2019-10-14 12:02:27 +0200

    compatibility

--- compatibility.js
+++ compatibility.js
@@ -1,1 +1,14 @@
-export const isCompatible = (window.safari === undefined) && (typeof WebGL2RenderingContext === 'function') && (document.createElement('canvas').getContext('webgl2') instanceof WebGL2RenderingContext);
+let pass;
+const gl = (typeof WebGL2RenderingContext === 'function') && document.createElement('canvas').getContext('webgl2');
+
+if (gl && gl instanceof WebGL2RenderingContext) {
+  const id = gl.createRenderbuffer();
+  gl.bindRenderbuffer(gl.RENDERBUFFER, id);
+  gl.renderbufferStorage(gl.RENDERBUFFER, 35056, 1, 1);
+  const error = gl.getError();
+  pass = error === gl.NO_ERROR;
+} else {
+  pass = false;
+}
+
+export const isCompatible = pass;
